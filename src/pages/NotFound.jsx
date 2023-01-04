9f53968f679017f3cf0e957f0058cf010cd3111c
Pratham <prathamsinghal2003@gmail.com>
2023-01-04 12:09:14 +0530

    :sparkles: Created new branch

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./random.css"
+import React from "react";
+import "./random.css";
 const NotFound = () => {
   return (
     <div id="err">
-        <h2>404</h2>
-        <p>Lost in Space</p>
+      <h2>404</h2>
+      <p>Lost</p>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
