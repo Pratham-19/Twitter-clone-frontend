0ddfb5dd011e727c05a5ae4eb886d1fa6dee7413
Pratham <prathamsinghal2003@gmail.com>
2023-01-04 12:05:39 +0530

    :art: Removed django files
    Deleted django files

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def main(request):
-    return HttpResponse("<h1>It's alive!</h1>")
-def signup(request):
-    return render(request, 'signup.html')
